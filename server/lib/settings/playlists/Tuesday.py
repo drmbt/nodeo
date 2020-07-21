6a7a7fc0a1680d6053b4d4af4dd60094a729bf26
Vincent Naples <DreamBaitrecordings@gmail.com>
2020-07-21 15:21:54 -0500

    drmbt fork Nodeo
    tests and assets

--- server/lib/settings/playlists/Tuesday.py
+++ server/lib/settings/playlists/Tuesday.py
@@ -1,11 +1,19 @@
 VidName	VidDur	TransName	TransDur	SndName	SndDur	Delete	VidPath	TransPath	SndPath
-abstractThing	28.0	XD	1.0	sndName	100.0		Media/Gen/abstractThing.tox	XD	sndPath
-arrakisStudy	28.0	XD	1.0	sndName	100.0		Media/Gen/arrakisStudy.tox	XD	sndPath
-bladerunner	28.0	XD	1.0	sndName	100.0		Media/Gen/bladerunner.tox	XD	sndPath
-cognitionCubes	28.0	XD	1.0	sndName	100.0		Media/Gen/cognitionCubes.tox	XD	sndPath
+RUMBLE SELECTS_1_1		XD	1.0	CHARLIE OTTO-A KING GRIEVES-MASTER-V1-MP3.mp3	None		Media/Mov/RUMBLE SELECTS_1_1.mov	XD	D:/github/nodeo/server/Media/Snd/CHARLIE OTTO-A KING GRIEVES-MASTER-V1-MP3.mp3
+Template TIME.53.2_1		XD	1.0	CHARLIE OTTO-A KING GRIEVES-MASTER-V1-MP3.mp3	None		Media/Mov/Template TIME.53.2_1.mov	XD	D:/github/nodeo/server/Media/Snd/CHARLIE OTTO-A KING GRIEVES-MASTER-V1-MP3.mp3
+Spiral Feedback Audio Grid_1		XD	1.0	sndName	100.0		Media/Mov/Spiral Feedback Audio Grid_1.mov	XD	sndPath
+RUMBLE SELECTS_1_1		XD	1.0	CHARLIE OTTO-A KING GRIEVES-MASTER-V1-MP3.mp3	None		Media/Mov/RUMBLE SELECTS_1_1.mov	XD	D:/github/nodeo/server/Media/Snd/CHARLIE OTTO-A KING GRIEVES-MASTER-V1-MP3.mp3
+Spiral Feedback Audio Grid_1		XD	1.0	sndName	100.0		Media/Mov/Spiral Feedback Audio Grid_1.mov	XD	sndPath
+RUMBLE SELECTS_1_1		XD	1.0	sndName	100.0		Media/Mov/RUMBLE SELECTS_1_1.mov	XD	sndPath
+RUMBLE SELECTS_1_1		interstitial2		sndName	100.0		Media/Mov/RUMBLE SELECTS_1_1.mov	Media/Trans/interstitial2.tox	sndPath
+silenceStudy	28.0	interstitial3		sndName	100.0		Media/Gen/silenceStudy.tox	Media/Trans/interstitial3.tox	sndPath
+pointcloud reactive feedback 1920x1080_1		XD	1.0	sndName	100.0		Media/Mov/pointcloud reactive feedback 1920x1080_1.mov	XD	sndPath
+arrakisStudy	28.0	interstitial6		sndName	100.0		Media/Gen/arrakisStudy.tox	Media/Trans/interstitial6.tox	sndPath
+bladerunner	28.0	nBumpr11		sndName	100.0		Media/Gen/bladerunner.tox	Media/Trans/nBumpr11.tox	sndPath
+cognitionCubes	28.0	nBumpr8		sndName	100.0		Media/Gen/cognitionCubes.tox	Media/Trans/nBumpr8.tox	sndPath
+RUMBLE SELECTS_1_1		nBumpr4		CHARLIE OTTO-A KING GRIEVES-MASTER-V1-MP3.mp3	None		Media/Mov/RUMBLE SELECTS_1_1.mov	Media/Trans/nBumpr4.tox	D:/github/nodeo/server/Media/Snd/CHARLIE OTTO-A KING GRIEVES-MASTER-V1-MP3.mp3
 moire	28.0	XD	1.0	sndName	100.0		Media/Gen/moire.tox	XD	sndPath
-okyotStudy	28.0	XD	1.0	sndName	100.0		Media/Gen/okyotStudy.tox	XD	sndPath
+okyotStudy	28.0	interstitial12		sndName	100.0		Media/Gen/okyotStudy.tox	Media/Trans/interstitial12.tox	sndPath
 reordreed	28.0	XD	1.0	sndName	100.0		Media/Gen/reordreed.tox	XD	sndPath
-silenceStudy	28.0	XD	1.0	sndName	100.0		Media/Gen/silenceStudy.tox	XD	sndPath
 flowerGems	28.0	XD	1.0	sndName	100.0		Media/Gen/flowerGems.tox	XD	sndPath
-colourStudy	28.0	XD	1.0	sndName	100.0		Media/Gen/colourStudy.tox	XD	sndPath
+colourStudy	28.0	interstitial9		sndName	100.0		Media/Gen/colourStudy.tox	Media/Trans/interstitial9.tox	sndPath
